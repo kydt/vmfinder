bba783993fb083fc993127ec6439bbe0199829f7
Lucky <kydtran@gmail.com>
2019-03-18 21:29:46 +0000

    Add database.js with user table and update server.js with user REST endpoints

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 // Create express app
 var express = require("express")
 var app = express()
+var db = require("./database.js")
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Server port
 var HTTP_PORT = 8000 
@@ -14,6 +19,65 @@ app.get("/", (req, res, next) => {
 });
 
 // Insert here other API endpoints
+
+//------------------------------------------api/users
+//GET all users
+app.get("/api/users", (req, res, next) => {
+    var sql = "select * from user"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+      });
+});
+
+//GET users by id
+app.get("/api/user/:id", (req, res, next) => {
+    var sql = "select * from user where id = ?"
+    var params = [req.params.id]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":row
+        })
+      });
+});
+
+//POST add user
+app.post("/api/user/", (req, res, next) => {
+    var errors=[]
+
+    if (errors.length){
+        res.status(400).json({"error":errors.join(",")});
+        return;
+    }
+    var data = {
+        name: req.body.name
+    }
+    var sql ='INSERT INTO user (name) VALUES (?)'
+    var params =[data.name]
+    db.run(sql, params, function (err, result) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data,
+            "id" : this.lastID
+        })
+    });
+})
 
 // Default response for any other request
 app.use(function(req, res){
